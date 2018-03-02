d247350cd5ac40bafa0b6ac6ff43b96128559a4d
rodyrobles <rodyrob@gmail.com>
2018-03-01 18:18:13 -0600

    Design of menus done

--- menuRegistroEmpleadosU5v1.0.c
+++ menuRegistroEmpleadosU5v1.0.c
@@ -1,62 +1,18 @@
 /*
-Programa: menuRegistroEmpleadosV3.0.c
+Programa: menuRegistroEmpleadosV1.0.c
 Programador: Rodolfo Gutierrez Robles
 Descripción: programa para el registro de empleados 
 Fecha de creación: 20 febrero, 2018
-Última modificación: 22 febrero, 2018
+Última modificación: 1 marzo, 2018
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 
-void dibujarMarco() {
-	//inicio dibujo de marco
-	gotoxy(1,1);			//esq sup izq    
-  	printf("%c",201); 		
-  	  	
-	int i; 					//borde superior
-	gotoxy(2,1);
-	for (i=0; i<68; i++) 
-	  	printf("%c",205); 
-  	
-  	int xVi=1, yVi=2;  		//vertical izquierda
-	int j;					
-	for (j=0; j<15; j++){
-		gotoxy(xVi,yVi);	
-		printf("%c",186);  
-		yVi= yVi+1;
-	}
-	
-	gotoxy(70,1);			//esq sup der
-	printf("%c",187); 		
-	
-	int xVd=70, yVd=2;		//vertical derecha
-	int k;					
-	for (k=0; k<15; k++){
-		gotoxy(xVd,yVd);	
-		printf("%c",186);  
-		yVd= yVd+1;
-	}
-	
-	gotoxy(2,6);  			//linea intermedia
-	int l;
-	for (l=0; l<68; l++)
-	printf("%c",205);  
-	
-	gotoxy(1,16);			//esq inf izq
-	printf("%c",200);
-	
-	gotoxy(2,16);  			//borde inferior
-	int m;
-	for (m=0; m<68; m++)
-	printf("%c",205); 
-	
-	gotoxy(70,16);			//esq inf der
-	printf("%c",188);
-	//fin dibujo de marco
+void dibujarMarco();
+void dibujarMarco2();
 
-}
 
 int main () {
 	
@@ -68,20 +24,15 @@ int main () {
 		 
 	dibujarMarco();
 	
-	gotoxy(25,3);
-	printf("RobleSys, S.A. de C.V.");
-	gotoxy(15,5);  
-	printf("Programa para el registro de empleados");
-	gotoxy(5,8);  
-	printf("Menu Principal");
-	gotoxy(5,10);
-	printf("1. Registro");
-	gotoxy(5,11);
-	printf("2. Consulta");
-	gotoxy(5,12);
-	printf("3. Salir\n");
-	gotoxy(5,14);
-	printf("Selecciona opcion: ");
+	gotoxy(25,3); printf("RobleSys, S.A. de C.V.");
+	gotoxy(15,5); printf("Programa para el registro de empleados");
+	highvideo();textbackground(GREEN);
+	gotoxy(5,8);  printf("Menu Principal");
+	lowvideo();textbackground(BLACK);
+	gotoxy(5,10); printf("1. Registro");
+	gotoxy(5,11); printf("2. Consulta");
+	gotoxy(5,12); printf("3. Salir\n");
+	gotoxy(5,14); printf("Selecciona opcion: ");
 	
 	scanf("%d", &select1);
 	
@@ -94,25 +45,26 @@ int main () {
 						
 			dibujarMarco();
 			
-			gotoxy(25,3);
-			printf("RobleSys, S.A. de C.V.");
-			gotoxy(15,5);  
-			printf("Programa para el registro de empleados");
-			gotoxy(5,8);  
-			printf("Menu registro de empleados");
-			gotoxy(5,10);
-			printf("1. Captura de datos");
-			gotoxy(5,11);
-			printf("2. Regresar al menu principal");
-			gotoxy(5,14);
-			printf("Selecciona opcion: ");
+			gotoxy(25,3); printf("RobleSys, S.A. de C.V.");
+			gotoxy(15,5); printf("Programa para el registro de empleados");
+			highvideo();textbackground(GREEN);
+			gotoxy(5,8);  printf("Menu registro de empleados");
+			lowvideo();textbackground(BLACK);
+			gotoxy(5,10); printf("1. Captura de datos");
+			gotoxy(5,11); printf("2. Regresar al menu principal");
+			gotoxy(5,14); printf("Selecciona opcion: ");
 						
 			scanf("%d", &select2);
 			
 			switch (select2) {
 				case 1:
 					system("cls");
-					printf("\t Registro de empleados \n\n");
+					dibujarMarco2();
+					gotoxy(25,3); printf("RobleSys, S.A. de C.V.");
+					gotoxy(15,5); printf("Programa para el registro de empleados");
+					highvideo();textbackground(GREEN);
+					gotoxy(25,8);  printf("Captura de datos\n\n");
+					lowvideo();textbackground(BLACK);
 					system ("pause");
 				break;
 				case 2:
@@ -127,25 +79,26 @@ int main () {
 			
 			dibujarMarco();
 			
-			gotoxy(25,3);
-			printf("RobleSys, S.A. de C.V.");
-			gotoxy(15,5);  
-			printf("Programa para el registro de empleados");
-			gotoxy(5,8);  
-			printf("Menu consulta de empleados");
-			gotoxy(5,10);
-			printf("1. Consulta de datos");
-			gotoxy(5,11);
-			printf("2. Regresar al menu principal");
-			gotoxy(5,14);
-			printf("Selecciona opcion: ");
+			gotoxy(25,3); printf("RobleSys, S.A. de C.V.");
+			gotoxy(15,5); printf("Programa para el registro de empleados");
+			highvideo();textbackground(GREEN);
+			gotoxy(5,8);  printf("Menu consulta de empleados");
+			lowvideo();textbackground(BLACK);
+			gotoxy(5,10); printf("1. Consulta de datos");
+			gotoxy(5,11); printf("2. Regresar al menu principal");
+			gotoxy(5,14); printf("Selecciona opcion: ");
 						
 			scanf("%d", &select3);
 						
 			switch (select3) {
 				case 1:
 					system("cls");
-					printf("\t Consulta de empleados \n\n");
+					dibujarMarco2();
+					gotoxy(25,3); printf("RobleSys, S.A. de C.V.");
+					gotoxy(15,5); printf("Programa para el registro de empleados");
+					highvideo();textbackground(GREEN);
+					gotoxy(25,8);  printf("Consulta de datos\n\n");
+					lowvideo();textbackground(BLACK);
 					system ("pause");
 				break;
 				case 2:
@@ -161,4 +114,100 @@ int main () {
 	system ("pause");
 	return 0;
 }
+
+
+
 		
+	void dibujarMarco() {
+	
+	system ("color 0a");
+	//inicio dibujo de marco
+	gotoxy(1,1); printf("%c",201); 		//esq sup izq    
+  	
+  	  	
+	int i; 					//borde superior
+	gotoxy(2,1);
+	
+	for (i=0; i<68; i++) 
+	  	printf("%c",205); 
+  	
+  	int xVi=1, yVi=2;  		//vertical izquierda
+	int j;					
+	for (j=0; j<15; j++){
+		gotoxy(xVi,yVi);	
+		printf("%c",186);  
+		yVi= yVi+1;
+	}
+	
+	gotoxy(70,1);	printf("%c",187);		//esq sup der
+	 		
+	
+	int xVd=70, yVd=2;		//vertical derecha
+	int k;					
+	for (k=0; k<15; k++){
+		gotoxy(xVd,yVd);	
+		printf("%c",186);  
+		yVd= yVd+1;
+	}
+	
+	gotoxy(2,6);  			//linea intermedia
+	int l;
+	for (l=0; l<68; l++)
+	printf("%c",205);  
+	
+	gotoxy(1,16);	printf("%c",200);		//esq inf izq
+	
+	
+	gotoxy(2,16);  			//borde inferior
+	int m;
+	for (m=0; m<68; m++)
+	printf("%c",205); 
+	
+	gotoxy(70,16);	printf("%c",188);		//esq inf der
+	
+	//fin dibujo de marco
+
+}
+
+
+void dibujarMarco2() {
+	
+	system ("color 0a");
+	//inicio dibujo de marco
+	gotoxy(1,1); printf("%c",201); 		//esq sup izq    
+  	
+  	int i; 					//borde superior
+	gotoxy(2,1);
+	
+	for (i=0; i<68; i++) 
+	  	printf("%c",205); 
+  	
+  	int xVi=1, yVi=2;  		//vertical izquierda
+	int j;					
+	for (j=0; j<4; j++){
+		gotoxy(xVi,yVi);	
+		printf("%c",186);  
+		yVi= yVi+1;
+	}
+	
+	gotoxy(70,1);	printf("%c",187);		//esq sup der
+	 		
+	int xVd=70, yVd=2;		//vertical derecha
+	int k;					
+	for (k=0; k<4; k++){
+		gotoxy(xVd,yVd);	
+		printf("%c",186);  
+		yVd= yVd+1;
+	}
+	
+	gotoxy(1,6);	printf("%c",200);		//esq inf izq
+	gotoxy(70,6);	printf("%c",188);		//esq inf der
+	
+	gotoxy(2,6);  			// Borde inferior
+	int l;
+	for (l=0; l<68; l++)
+	printf("%c",205);  
+	
+	//fin dibujo de marco2
+}
+
